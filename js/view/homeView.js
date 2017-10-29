e262c83058eed52abb87d8c5a6205ceb7e720e62
mohit493 <mohityadav0493@gmail.com>
2017-10-29 19:54:06 +0100

    views are now complete

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -3,13 +3,7 @@ var HomeView = function (container,model) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	this.numberOfGuests = container.find("#numberOfGuests");
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
-	
-
-
-	this.numberOfGuests.html(model.getNumberOfGuests());
+	this.createNewDinner = container.find("#createNewButton");
 	
 }
  
--- js/view/selectDishView.js
+++ js/view/selectDishView.js
@@ -3,11 +3,15 @@ var SelectDishView = function (container,model) {
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
+	
+  	this.title = container.find("#titleTest");
 	this.numberOfGuests = container.find("#numberOfGuests");
 	this.plusButton = container.find("#plusGuest");
 	this.minusButton = container.find("#minusGuest");
 
 	console.log(model.addDishToMenu(201));
+
+	this.title.html(Selected);
 	
 
 
