40ff5f89751936b421904939097abfb3ea045ef1
mohit493 <mohityadav0493@gmail.com>
2018-03-07 12:01:39 +0100

    MVC base complete

--- js/view/sideView.js
+++ js/view/sideView.js
@@ -4,20 +4,22 @@ var SideView = function (container,model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction
   	model.addDishToMenu(1);
-	model.addDishToMenu(100);
+  	model.addDishToMenu(100);
+  	model.addDishToMenu(200);
 
 
-	var numOfGuest = model.getNumberOfGuests();
-	var totalPrice = model.getTotalMenuPrice();
-	var currentMenu = model.getFullMenu();
+
+  	var numOfGuest = model.getNumberOfGuests();
+  	var totalPrice = model.getTotalMenuPrice();
+  	var currentMenu = model.getFullMenu();
 
 	//var numOfGuestField = document.getElementById('#NoOfGuestField').value;
 
-	this.totalMenuPriceDisplay = container.find("#totalMenuPrice");
+	var totalMenuPriceDisplay = container.find("#totalMenuPrice");
 	var totalGuestNumberDisplay = container.find("#NoOfGuestField");
-	this.StarterOnMenuDisplay = container.find("#StarterDishOnMenu");
-	this.MCOnMenuDisplay = container.find("#MCDishOnMenu");
-	this.DessertOnMenuDisplay = container.find("#DessertDishOnMenu");
+	var StarterOnMenuDisplay = container.find("#StarterDishOnMenu");
+	var MCOnMenuDisplay = container.find("#MCDishOnMenu");
+	var DessertOnMenuDisplay = container.find("#DessertDishOnMenu");
 	this.PlusButton = container.find("#plusGuest");
 	this.MinusButton = container.find("#minusGuest")
 
@@ -29,60 +31,104 @@ var SideView = function (container,model) {
 	var DessertOnMenu = '';
 
 
-	totalMenuPriceInfo = '<p> SEK '+ totalPrice + '</p>';
-
-	if(currentMenu[0].id == undefined)
-	{
-		StarterOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+ ' ....' +'</p></div>';
-	}
-	else
-	{
-		StarterOnMenu = '<div class="col-md-6" > <p>' + currentMenu[0].name + ' </p> </div>'+'<div class="col-md-6"><p>'+model.getDishPrice(currentMenu[0].id)+' x '+numOfGuest+'</p></div>';
-
-	}
-
-	if(currentMenu[1].id == undefined)
-	{
-	MCOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+ '....'+'</p></div>';
-	}
-	else
-	{
-	MCOnMenu = '<div class="col-md-6" > <p>' + currentMenu[1].name + ' </p> </div>'+'<div class="col-md-6"><p>'+ model.getDishPrice(currentMenu[1].id)+' x '+numOfGuest+'</p></div>';
-
-	}
-
-	if(currentMenu[2].id == undefined)
-	{
-	DessertOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+'....'+'</p></div>';
-	}
-	else
-	{
-	DessertOnMenu = '<div class="col-md-6" > <p>' + currentMenu[2].name + ' </p> </div>'+'<div class="col-md-6"><p>'+ model.getDishPrice(currentMenu[2].id)+' x '+numOfGuest+'</p></div>';
-
-	}
-
-	this.totalMenuPriceDisplay.html(totalMenuPriceInfo);
-	this.StarterOnMenuDisplay.html(StarterOnMenu);
-	this.MCOnMenuDisplay.html(MCOnMenu);
-	this.DessertOnMenuDisplay.html(DessertOnMenu);
-
 	var ObserverObj = {
 		name: "SideView",
 		update: function(arg){
 			if(arg == "GuestChange")
 			{
-			console.log("altered version running");
+				console.log("altered version running");
 
-			numOfGuest = model.getNumberOfGuests();
-			numOfGuestFieldInfo = '<input type = "number" value = "'+numOfGuest+'">';
-			totalGuestNumberDisplay.html(numOfGuestFieldInfo);
+				numOfGuest = model.getNumberOfGuests();
+				totalPrice = model.getTotalMenuPrice();
+
+				numOfGuestFieldInfo = '<input type = "number" value = "'+numOfGuest+'">';
+
+				totalMenuPriceInfo = '<p> SEK '+ totalPrice + '</p>';
+
+
+				if(currentMenu[0].id == undefined)
+				{
+					StarterOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+ ' ....' +'</p></div>';
+				}
+				else
+				{
+					StarterOnMenu = '<div class="col-md-6" > <p>' + currentMenu[0].name + ' </p> </div>'+'<div class="col-md-6"><p>'+model.getDishPrice(currentMenu[0].id)+' x '+numOfGuest+'</p></div>';
+
+				}
+
+				if(currentMenu[1].id == undefined)
+				{
+					MCOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+ '....'+'</p></div>';
+				}
+				else
+				{
+					MCOnMenu = '<div class="col-md-6" > <p>' + currentMenu[1].name + ' </p> </div>'+'<div class="col-md-6"><p>'+ model.getDishPrice(currentMenu[1].id)+' x '+numOfGuest+'</p></div>';
+
+				}
+				if(currentMenu[2].id == undefined)
+				{
+					DessertOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+'....'+'</p></div>';
+				}
+				else
+				{
+					DessertOnMenu = '<div class="col-md-6" > <p>' + currentMenu[2].name + ' </p> </div>'+'<div class="col-md-6"><p>'+ model.getDishPrice(currentMenu[2].id)+' x '+numOfGuest+'</p></div>';
+
+				}
+
+				totalMenuPriceDisplay.html(totalMenuPriceInfo);
+				totalGuestNumberDisplay.html(numOfGuestFieldInfo);
+				StarterOnMenuDisplay.html(StarterOnMenu);
+				MCOnMenuDisplay.html(MCOnMenu);
+				DessertOnMenuDisplay.html(DessertOnMenu);
+
 			}
 			else
 			{
 				console.log("base version running");
-			numOfGuest = model.getNumberOfGuests();
-			numOfGuestFieldInfo = '<input type = "number" value = "'+numOfGuest+'">';
-			totalGuestNumberDisplay.html(numOfGuestFieldInfo);
+				numOfGuest = model.getNumberOfGuests();
+				totalPrice = model.getTotalMenuPrice();
+
+				numOfGuestFieldInfo = '<input type = "number" value = "'+numOfGuest+'">';
+
+				totalMenuPriceInfo = '<p> SEK '+ totalPrice + '</p>';
+
+
+				if(currentMenu[0].id == undefined)
+				{
+					StarterOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+ ' ....' +'</p></div>';
+				}
+				else
+				{
+					StarterOnMenu = '<div class="col-md-6" > <p>' + currentMenu[0].name + ' </p> </div>'+'<div class="col-md-6"><p>'+model.getDishPrice(currentMenu[0].id)+' x '+numOfGuest+'</p></div>';
+
+				}
+
+				if(currentMenu[1].id == undefined)
+				{
+					MCOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+ '....'+'</p></div>';
+				}
+				else
+				{
+					MCOnMenu = '<div class="col-md-6" > <p>' + currentMenu[1].name + ' </p> </div>'+'<div class="col-md-6"><p>'+ model.getDishPrice(currentMenu[1].id)+' x '+numOfGuest+'</p></div>';
+
+				}
+				if(currentMenu[2].id == undefined)
+				{
+					DessertOnMenu = '<div class="col-md-6" > <p>' + '---' + ' </p> </div>'+'<div class="col-md-6"><p>'+'....'+'</p></div>';
+				}
+				else
+				{
+					DessertOnMenu = '<div class="col-md-6" > <p>' + currentMenu[2].name + ' </p> </div>'+'<div class="col-md-6"><p>'+ model.getDishPrice(currentMenu[2].id)+' x '+numOfGuest+'</p></div>';
+
+				}
+
+				totalMenuPriceDisplay.html(totalMenuPriceInfo);
+				totalGuestNumberDisplay.html(numOfGuestFieldInfo);
+				StarterOnMenuDisplay.html(StarterOnMenu);
+				MCOnMenuDisplay.html(MCOnMenu);
+				DessertOnMenuDisplay.html(DessertOnMenu);
+
+
 			}
 			
 		}
